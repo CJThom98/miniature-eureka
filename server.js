efc42490cd13828b5432d2c4ea9f9640077efb10
Cory Thompson <cory.thompson98@gmail.com>
2022-01-27 17:19:30 -0600

    MVP

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { db } = require('./data/db');
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, dbArray) {
@@ -13,6 +14,11 @@ function filterByQuery(query, dbArray) {
     return filteredResults;
 }
 
+function findById(id, dbArray) {
+    const result = dbArray.filter(db => db.id === id)[0];
+    return result;
+}
+
 app.get('/api/db', (req, res) => {
     let results = db;
     if (req.query) {
@@ -21,6 +27,15 @@ app.get('/api/db', (req, res) => {
     res.json(results);
 });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.get('/api/db/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result); 
+    } else {
+        res.send(404);
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
