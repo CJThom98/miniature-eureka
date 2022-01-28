fff57ce5c6cb93b6d20a9d88a1055257922f3132
Cory Thompson <cory.thompson98@gmail.com>
2022-01-28 13:58:53 -0600

    route

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -58,6 +59,18 @@ app.post('/api/db', (req, res) => {
     const note = createNewNote(req.body, db);
 
     res.json(note);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
 });
 
 app.listen(PORT, () => {
